16a7ed431d5d3d188b0069a3398af7f64a7e4616
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2021-01-25 17:43:36 +0700

    readme

--- app/src/main/java/com/gzeinnumer/mylibrecyclerviewadapterbuilderexamplekt/MainActivity.kt
+++ app/src/main/java/com/gzeinnumer/mylibrecyclerviewadapterbuilderexamplekt/MainActivity.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import android.os.CountDownTimer
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.gzeinnumer.mylibrecyclerviewadapterbuilder.AdapterBuilder
+import com.gzeinnumer.mylibrecyclerviewadapterbuilder.helper.BindViewHolderMultiType
+import com.gzeinnumer.mylibrecyclerviewadapterbuilder.model.TypeViewItem
+import com.gzeinnumer.mylibrecyclerviewadapterbuilder.multiType.AdapterBuilderMultiType
+import com.gzeinnumer.mylibrecyclerviewadapterbuilder.singleType.AdapterBuilder
 import com.gzeinnumer.mylibrecyclerviewadapterbuilderexamplekt.databinding.ActivityMainBinding
 import com.gzeinnumer.mylibrecyclerviewadapterbuilderexamplekt.databinding.RvItemBinding
+import com.gzeinnumer.mylibrecyclerviewadapterbuilderexamplekt.databinding.RvItemGenapBinding
 import java.util.*
 
 
@@ -21,10 +26,11 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        initAdapter()
+        typeSingle()
+//        typeMulti()
     }
 
-    private fun initAdapter() {
+    private fun typeSingle() {
         val list: MutableList<MyModel> = ArrayList()
         for (i in 0..9) {
             list.add(MyModel(i, "Data Ke " + (i + 1)))
@@ -57,6 +63,84 @@ class MainActivity : AppCompatActivity() {
                             fildteredList.add(item)
                         }
                         if (item.name.toString().toLowerCase().contains(filterPattern)) {
+                            fildteredList.add(item)
+                        }
+                    }
+                }
+                fildteredList
+            }
+
+        binding.rv.adapter = adapter
+        binding.rv.layoutManager = LinearLayoutManager(applicationContext)
+        binding.rv.hasFixedSize()
+
+        //after 5 second, new data will appear
+        object : CountDownTimer(5000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {}
+            override fun onFinish() {
+                for (i in 10..100) {
+                    list.add(MyModel(i, "Data Ke " + (i + 1)))
+                }
+                adapter.setList(list)
+            }
+        }.start()
+
+        //use filter on TextWacher
+        binding.ed.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable) {
+                //call the filter
+                adapter.filter.filter(s)
+            }
+        })
+    }
+
+    private fun typeMulti() {
+        val list: MutableList<MyModel> = ArrayList()
+        for (i in 0..9) {
+            list.add(MyModel(i, "Data Ke " + (i + 1)))
+        }
+
+        val adapter = AdapterBuilderMultiType<MyModel>()
+            .setList(list)
+            .setCustomNoItem(R.layout.custom_empty_item)
+            .setAnimation(R.anim.anim_two)
+            .setDivider(R.layout.custom_divider)
+            .onBind(object : BindViewHolderMultiType<MyModel> {
+
+                private val TYPE_GENAP = 1
+                private val TYPE_GANJIL = 0
+
+                override fun getItemViewType(position: Int): TypeViewItem {
+                    return if (position % 2 == 0)
+                        TypeViewItem(TYPE_GENAP, R.layout.rv_item_genap)
+                    else
+                        TypeViewItem(TYPE_GANJIL, R.layout.rv_item)
+                }
+
+                override fun bind(holder: View, data: MyModel, position: Int, viewType: Int) {
+                    if (viewType == TYPE_GENAP) {
+                        val bindingItem = RvItemGenapBinding.bind(holder)
+                        bindingItem.btn.text = data.id.toString() + "_" + data.name + "_Genap"
+                    } else if (viewType == TYPE_GANJIL) {
+                        val bindingItem = RvItemBinding.bind(holder)
+                        bindingItem.btn.text = data.id.toString() + "_" + data.name + "_Ganjil"
+                    }
+                }
+            })
+            .onFilter { constraint, listFilter ->
+                val fildteredList: MutableList<MyModel> = ArrayList()
+
+                if (constraint.isNotEmpty()) {
+                    val filterPattern = constraint.toString().toLowerCase().trim { it <= ' ' }
+                    for (item in listFilter) {
+                        if (item.id.toString().toLowerCase().contains(filterPattern)) {
+                            fildteredList.add(item)
+                        }
+                        if (item.name.toLowerCase().contains(filterPattern)) {
                             fildteredList.add(item)
                         }
                     }
